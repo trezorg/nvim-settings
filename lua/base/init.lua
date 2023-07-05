0fc3b7c31959b984e318ac1fc5609388f713a9e8
Igor Nemilentsev <trezorg@gmail.com>
2023-07-05 22:28:26 +0300

    Updated lua/base/init.lua

--- lua/base/init.lua
+++ lua/base/init.lua
@@ -15,7 +15,7 @@ return {
     opts = {
       show_index = true, -- show tab index
       show_modify = true, -- show buffer modification indicator
-      show_icon = false, -- show file extension icon
+      show_icon = true, -- show file extension icon
       fnamemodify = ':t', -- file name modifier
       modify_indicator = '[+]', -- modify indicator
       no_name = 'No name', -- no name buffer name
