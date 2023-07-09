36aed55d30f24b69a1ee0af1145873a133e60be4
Igor Nemilentsev <trezorg@gmail.com>
2023-07-10 00:07:56 +0300

    ansible filetype

--- lua/pde/ansible.lua
+++ lua/pde/ansible.lua
@@ -15,6 +15,24 @@ return {
     init = function()
       vim.g.ansible_attribute_highlight = 'ob'
       vim.g.ansible_name_highlight = 'd'
+      vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+        pattern = { 'hosts.yaml', 'hosts', 'hosts.yml' },
+        callback = function()
+          -- if vim.fn.search('{{.\\+}}', 'nw') ~= 0 then
+          local buf = vim.api.nvim_get_current_buf()
+          vim.api.nvim_buf_set_option(buf, 'filetype', 'yaml.ansible')
+          -- end
+        end,
+      })
+      vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+        pattern = { '*.yaml', '*.yml' },
+        callback = function()
+          if vim.fn.match(vim.fn.trim(vim.fn.getline(1)), '# code: language=ansible') >= 0 then
+            local buf = vim.api.nvim_get_current_buf()
+            vim.api.nvim_buf_set_option(buf, 'filetype', 'yaml.ansible')
+          end
+        end,
+      })
     end,
   },
   {
