fa85286945431fff2884b843bb3fe7510e668b20
Igor Nemilentsev <trezorg@gmail.com>
2024-06-22 21:21:11 +0300

    venv-selector branch

--- lua/pde/python.lua
+++ lua/pde/python.lua
@@ -36,6 +36,7 @@ return {
   },
   {
     "linux-cultist/venv-selector.nvim",
+    branch = "regexp",
     dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
     event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
     keys = {
