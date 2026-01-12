a729bc5f8e15a98b9ec22a7dde991b0f2b9fbf94
Igor Nemilentsev <trezorg@gmail.com>
2026-01-12 17:35:07 +0300

    neotree

--- lua/base/init.lua
+++ lua/base/init.lua
@@ -55,6 +55,21 @@ return {
         enable_diagnostics = true,
         open_files_do_not_replace_types = { 'terminal', 'trouble', 'qf' },
         sort_case_insensitive = false,
+        filesystem = {
+          filtered_items = {
+            visible = true,
+            show_hidden_count = true,
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            hide_by_name = {
+              -- add extension names you want to explicitly exclude
+              -- '.git',
+              -- '.DS_Store',
+              -- 'thumbs.db',
+            },
+            never_show = {},
+          },
+        },
         default_component_configs = {
           indent = {
             indent_size = 2,
