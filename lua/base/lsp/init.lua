0c1563438f491c8a8db9227e07c271e1dbeeae85
Igor Nemilentsev <trezorg@gmail.com>
2026-02-24 12:22:53 +0300

    Fixed depricated log

--- lua/base/lsp/init.lua
+++ lua/base/lsp/init.lua
@@ -60,7 +60,7 @@ return {
     },
     config = function(plugin, opts)
       require('base.lsp.servers').setup(plugin, opts)
-      vim.lsp.set_log_level 'error'
+      vim.lsp.log.set_level 'error'
     end,
   },
   {
